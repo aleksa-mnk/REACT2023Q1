94926b6ec187408634de3415f71437b83dffe4c0
aleksa-mnk <alyonamankevich@gmail.com>
2023-04-22 16:39:57 +0300

    init: start task

--- src/__tests__/components/Form.test.tsx
+++ src/__tests__/components/Form.test.tsx
@@ -1,14 +1,32 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import store from '../../core/store/store';
 import { Form } from '../../components/Form/Form';
-import { render, screen } from '@testing-library/react';
-import { describe, expect, it } from 'vitest';
 
-/**
- * @vitest-environment jsdom
- */
+describe('Form component', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
 
-describe('Test Form component', () => {
-  it('renders with correct text', () => {
-    render(<Form />);
-    expect(screen.getByText('Snack Name')).toBeInTheDocument();
+  it('should render the form and submit a new snack', () => {
+    render(
+        <Provider store={store}>
+          <Form />
+        </Provider>
+    );
+
+    const nameInput = screen.getByLabelText(/name/i);
+    const imageInput = screen.getByLabelText(/image/i);
+    const descriptionInput = screen.getByLabelText(/description/i);
+    const addButton = screen.getByRole('button', { name: /add/i });
+
+    fireEvent.change(nameInput, { target: { value: 'Test Snack' } });
+    fireEvent.change(imageInput, { target: { value: 'http://example.com/image.png' } });
+    fireEvent.change(descriptionInput, { target: { value: 'This is a test snack.' } });
+
+    fireEvent.click(addButton);
+
+    const snackName = screen.getByText(/Test Snack/i);
+    expect(snackName).toBeInTheDocument();
   });
 });
